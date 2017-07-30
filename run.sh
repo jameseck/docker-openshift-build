5090ff9b80546a6f0c3e7b02b6cd0ed332cb51df
James Eckersall <james.eckersall@gmail.com>
2017-07-30 19:28:13 +0100

    change to run script

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 GIT_BRANCH=release-1.5
-RPM_VERSION=1.5.1
-RPM_RELEASE=1
 
 git config --global user.name "James Eckersall"
 git config --global user.email "james.eckersall@gmail.com"
@@ -12,6 +10,4 @@ go get -v github.com/openshift/origin
 cd /go/src/github.com/openshift/origin
 git checkout $GIT_BRANCH
 yum-builddep -y origin.spec
-
-tito tag --use-version=${RPM_VERSION} --use-release=${RPM_RELEASE} --no-auto-changelog
-tito build --test --rpm --dist=el7
+make build-rpms
